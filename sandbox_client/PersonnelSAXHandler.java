13322ef61b71d1730bfa8caf8e51b6726c71e35d
Richard <rcloutier778@gmail.com>
2016-06-30 11:14:06 -0400

    Updated local to match git, no actual changes.

--- sandbox_client/PersonnelSAXHandler.java
+++ sandbox_client/PersonnelSAXHandler.java
@@ -2,21 +2,21 @@ package sandbox_client;
 
 /**
  * Reads information from the personnel.xml file and sends the information back to the Database.
- * 
+ *
  * @author dmayans
  */
 
-import java.io.IOException;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.*;
-import org.xml.sax.helpers.*;
+import java.io.IOException;
 
 public class PersonnelSAXHandler extends DefaultHandler {
-	
+
 	// Called at the beginning of the program to read the personnel.xml file
 	public static void generatePersonnel() {
 		String s = Main.PATH_TO_ASSETS + "personnel.xml";
@@ -31,18 +31,18 @@ public class PersonnelSAXHandler extends DefaultHandler {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String _string;
 	private String _color;
 	private String _name;
 	private String _effect;
 	private String _tier;
-	
+
 	@Override
 	public void startDocument() {
 		_string = "";
 	}
-	
+
 	@Override
 	public void startElement(String namespaceURI, String localName, String qName, Attributes atts) {
 		if(qName=="person") {
@@ -50,7 +50,7 @@ public class PersonnelSAXHandler extends DefaultHandler {
 			_tier = atts.getValue("tier");
 		}
 	}
-	
+
 	@Override
 	public void endElement(String uri, String localName, String qName) {
 		if(qName.equals("name")) {
@@ -62,15 +62,15 @@ public class PersonnelSAXHandler extends DefaultHandler {
 		}
 		_string = "";
 	}
-	
+
 	@Override
 	public void characters(char[] ch, int start, int length) {
 		_string += new String(ch, start, length).trim();
 	}
-	
+
 	@Override
 	public void endDocument() {
-		
+
 	}
-	
+
 }
--- sandbox_client/Tile.java
+++ sandbox_client/Tile.java
@@ -3,7 +3,7 @@ package sandbox_client;
 /**
  * Models a tile- the information about it (what groups it belongs in, planetary information), etc.
  * Also keeps track of its image pathname for Hexagons to paint.
- * 
+ *
  * @author dmayans
  */
 
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 public class Tile {
-			
+
 	// Called at the beginning of the program to read the planets.xml file
 	public static void generateTiles() {
 		String s = Main.PATH_TO_ASSETS + "planets.xml";
@@ -32,45 +32,45 @@ public class Tile {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// Called after reading the planets.xml file to place the tiles in their array
 	public static void placeTiles(LinkedList<Tile> tiles) {
 		for(Tile t : tiles) {
 			Database.addTile(t);
 		}
 	}
-	
+
 	// General information about the tile
 	private String _path;
 	private String _title;
-	
+
 	private Planet[] _planets = {};
-	
+
 	// tile accessors
 	public String getPath() {
 		return "file:TIMEGA/assets/Systems/" + _path;
 	}
-	
+
 	public String getTitle() {
 		return _title;
 	}
-	
+
 	// tile mutators
 	public void setPath(String path) {
 		_path = path;
 	}
-	
+
 	public void setTitle(String title) {
 		_title = title;
 	}
-	
+
 	// planet accessors
 	public String getPlanetName(int index) {
-		if(index >= _planets.length || index < 0) 
+		if (index >= _planets.length || index < 0)
 			return null;
 		return _planets[index].name;
 	}
-	
+
 	public int hasPlanet(String name) {
 		for(int i = 0; i < _planets.length; i++) {
 			if(_planets[i].name.equals(name)) {
@@ -79,25 +79,25 @@ public class Tile {
 		}
 		return -1;
 	}
-	
+
 	public int getPlanetResources(int index) {
 		if(index >= _planets.length || index < 0) {
 			return -1;
 		}
 		return _planets[index].resources;
 	}
-	
+
 	public int getPlanetInfluence(int index) {
-		if(index >= _planets.length || index < 0) 
+		if (index >= _planets.length || index < 0)
 			return -1;
 		return _planets[index].influence;
 	}
-	
+
 	public int numPlanets() {
 		return _planets.length;
 	}
-	
-	
+
+
 	// planet mutators
 	public void addPlanet() {
 		int newlength = _planets.length + 1;
@@ -108,35 +108,35 @@ public class Tile {
 		newplanets[_planets.length] = new Planet();
 		_planets = newplanets;
 	}
-	
+
 	public void setPlanetName(int index, String name) {
 		Database.addPlanetName(name);
 		_planets[index].name = name;
 	}
-	
+
 	public void setPlanetResources(int index, String resources) {
 		int r = Integer.parseInt(resources);
 		Database.setResources(_planets[index].name, r);
 		_planets[index].influence = r;
 	}
-	
+
 	public void setPlanetInfluence(int index, String influence) {
 		int i = Integer.parseInt(influence);
 		Database.setInfluence(_planets[index].name, i);
 		_planets[index].influence = i;
 	}
-	
+
 	public void addTechSpec(int index, int tech) {
 		Database.addTechSpec(_planets[index].name, tech);
 	}
-	
-	
+
+
 	private class Planet {
-		
+
 		public String name;
 		public int resources;
 		public int influence;
-				
+
 	}
-	
+
 }
