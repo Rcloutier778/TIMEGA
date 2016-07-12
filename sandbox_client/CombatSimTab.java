e3b525b72023e33e7e1e0d79dcec7bda58650f93
Richard <rcloutier778@gmail.com>
2016-07-12 14:52:38 -0400

    Updated UI, minor refactoring, minor bugfixes. Player and Enemy are now Attacker and Defender. Can choose both from drop down list.

--- sandbox_client/CombatSimTab.java
+++ sandbox_client/CombatSimTab.java
@@ -2,9 +2,11 @@ package sandbox_client;
 
 import javafx.application.Platform;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
@@ -27,6 +29,25 @@ import java.math.BigDecimal;
  * 5) Custom targeting order
  */
 
+/**
+ * Current problems with simulator tab:
+
+ - there is no distinction between attacker and defender (important in some cases)
+
+ Recommendations:
+ - leave the results text in permanently (on the right half of the screen), and just change the output numbers when the simulation is run
+ - run the simulator immediately after someone finishes editing the box instead of having to hit the button (the simulation is fast enough anyway)
+ - allow the user to change each player's loss policy; while I agree that yours should be the default, that customization is something that I know that Dan and I would be disappointed to miss out on
+
+ Further work:
+ - flagships are starting to roll in, I'll let you know when the database can handle it
+ - home system/adjacent combat bonus
+ - updated racial abilities (those are starting to roll in too, might want to take a peek)
+ - add in SASs (don't do this yet, wait until they appear in the rulebook)
+ - make sure the simulator is appropriately handling the current personnel and technology (it should be pretty stable right now, only changing stuff with SASs in a bit)
+
+ */
+
 public class CombatSimTab extends AbstractTab {
 
     // Units fields
@@ -44,11 +65,7 @@ public class CombatSimTab extends AbstractTab {
     private static final int PLAYER = 0;
     private static final int ENEMY = 1;
 
-    private Button _start;
-    private GridPane _scenepane = new GridPane();
-    private Text _playerName;
-    private ComboBox<String> _eOptions;
-    private GridPane _pane;
+    private ComboBox<String>[] _eOptions = new ComboBox[]{new ComboBox<String>(), new ComboBox<String>()};
 
     //name of player(0) and enemy(1)
     public String[] _names = new String[2];
@@ -57,28 +74,50 @@ public class CombatSimTab extends AbstractTab {
     public CombatSimTab() {
         super(Client.SIMULATOR);
         _root.setClosable(false);
+        GridPane _scenepane = new GridPane();
         _root.setContent(_scenepane);
 
-        _pane = new GridPane();
+        GridPane _pane = new GridPane();
         Text results = new Text();
         results.setText("Enter data to view results");
         Text resultTitle = new Text();
         resultTitle.setText("Result of the battle:");
 
-        //make new textfields
+        Label[] _unitLabels = new Label[5];
+
         for(int k=0; k<Database.NUM_SHIPS; k++){
+            //Make new Number Text Fields and labels
             _unitFields[PLAYER][k] = new NumberTextField();
             _unitFields[ENEMY][k] = new NumberTextField();
-        }
+            _unitLabels[k] = new Label();
 
-        //Set the prompt text
-        for(int k=0; k<Database.NUM_SHIPS; k++){
+            //Set the prompt text
             _unitFields[PLAYER][k].setPromptText(Database.nameOfShip(k) + "s");
             _unitFields[ENEMY][k].setPromptText(Database.nameOfShip(k) + "s");
+
+            //Set the label names
+            _unitLabels[k].setText(Database.nameOfShip(k) + "s:");
+
+            _unitFields[PLAYER][k].setMaxWidth(90);
+            _unitFields[ENEMY][k].setMaxWidth(90);
+
+            //Add things to the grid
+            _pane.add(_unitLabels[k], 1, k + 2);
+            _pane.add(_unitFields[PLAYER][k], 2, k + 2);
+            _pane.add(_unitFields[ENEMY][k], 3, k + 2);
         }
 
+
+        GridPane[] _namePane = {new GridPane(), new GridPane()};
+
+        _namePane[PLAYER].add(new Text("Attacker"),1,1);
+        _namePane[PLAYER].add(_eOptions[PLAYER],1,2);
+
+        _namePane[ENEMY].add(new Text("Defender"),1,1);
+        _namePane[ENEMY].add(_eOptions[ENEMY],1,2);
+
         //start button
-        _start = new Button("Start");
+        Button _start = new Button("Start");
         _start.setOnAction(e ->
                 Platform.runLater(new Runnable() {
                     @Override
@@ -88,35 +127,29 @@ public class CombatSimTab extends AbstractTab {
                 }));
         GridPane.setHalignment(_start, HPos.CENTER);
 
-        _eOptions = new ComboBox<String>();
-
-        //Add things to grid
-        for(int k=0; k<Database.NUM_SHIPS; k++){
-            _pane.add(_unitFields[PLAYER][k], 1, k + 2);
-            _pane.add(_unitFields[ENEMY][k], 2, k + 2);
-        }
 
         _pane.setAlignment(Pos.CENTER);
 
         GridPane ResultsPane = new GridPane();
         ResultsPane.add(resultTitle,1,1);
         ResultsPane.add(results,1,2);
-        ResultsPane.setMinWidth(240);
+        ResultsPane.setMinWidth(300);
         ResultsPane.setVgap(40);
 
         _pane.setVgap(40);
         _pane.setHgap(60);
-        _playerName = new Text();
-        _playerName.setText("Click start");
 
-        _pane.add(_playerName, 1, 1);
-        _pane.add(_eOptions, 2, 1);
-        _pane.add(_start, 2, 7);
+        //Player and enemey names
+        _pane.add(_namePane[PLAYER], 2, 1);
+        _pane.add(_namePane[ENEMY], 3, 1);
+
+        _pane.add(_start, 1, 1);
         _scenepane.add(ResultsPane, 2, 1);
-        _scenepane.setAlignment(Pos.CENTER);
         _scenepane.add(_pane, 1, 1);
-
+        _scenepane.setPadding(new Insets(0,180,60,0));
         _scenepane.setHgap(60);
+        _scenepane.setAlignment(Pos.CENTER);
+
     }
 
     /**
@@ -124,17 +157,12 @@ public class CombatSimTab extends AbstractTab {
      */
     @Override
     public void localName(String name) {
-        // Gets the name of the client (player)
-        _playerName.setText(name);
-        // Gets the name of the opposing players
-        // drop down menu for enemies
-        if (_eOptions.getItems().isEmpty()) {
+        // Gets the name of the players
+        if (_eOptions[0].getItems().isEmpty()) {
             for (int i = 0; i < Database.numPlayers(); i++) {
-                if(!name.equals(Database.getPlayer(i).name)){
-                    _eOptions.getItems().add(Database.getPlayer(i).name);
-                }
+                _eOptions[PLAYER].getItems().add(Database.getPlayer(i).name);
+                _eOptions[ENEMY].getItems().add(Database.getPlayer(i).name);
             }
-            _eOptions.getItems().remove(_playerName.getText());
         }
     }
 
@@ -158,8 +186,8 @@ public class CombatSimTab extends AbstractTab {
      */
     public void damageVal () {
         for(int i = 0; i < 2; i++) {
-            if (Database.hasTechLocal(_names[i], "Hylar V Assult Laser")) {
-                _unitHitRate[i][Database.DESTROYER] -= 1;
+            if (Database.hasTechLocal(_names[i], "Hylar V Assault Laser")) {
+                _unitHitRate[i][Database.DESTROYER]--;
             }
             
             if (Database.hasTechLocal(_names[i], "Ion Cannons")) {
@@ -167,21 +195,21 @@ public class CombatSimTab extends AbstractTab {
             }
             
             if(Database.hasTechLocal(_names[i], "Cybernetics")) {
-                _unitHitRate[i][Database.FIGHTER] -= 1;
+                _unitHitRate[i][Database.FIGHTER]--;
             }
             
             if(Database.hasTechLocal(_names[i], "Advanced Fighters")) {
-                _unitHitRate[i][Database.FIGHTER] -= 1;
+                _unitHitRate[i][Database.FIGHTER]--;
             }
             
             if(Database.raceOf(_names[i]).equals("The Sardakk N'Orr")){
-                for(int k=0; i<Database.NUM_SHIPS; k++) {
-                    _unitHitRate[i][k] -= 1;
+                for(int k=0; k<Database.NUM_SHIPS; k++) {
+                    _unitHitRate[i][k]--;
                 }
             }
             
             if(Database.raceOf(_names[i]).equals("The L1Z1X Mindnet")){
-                _unitHitRate[i][Database.DREADNOUGHT] -= 1;
+                _unitHitRate[i][Database.DREADNOUGHT]--;
             }
         }
     }
@@ -217,7 +245,7 @@ public class CombatSimTab extends AbstractTab {
                     while(ADT < _unitCounts[i][Database.DESTROYER] || ADT < _unitCounts[e][Database.FIGHTER]) {
                         for (int k = 0; k < _unitCounts[e][Database.FIGHTER] / 4; k++) {
                             if (diceRoller() >= (_unitHitRate[i][Database.FIGHTER] - 1)) {
-                                ADT += 1;
+                                ADT++;
                             }
                         }
                     }
@@ -235,30 +263,24 @@ public class CombatSimTab extends AbstractTab {
             if (Database.hasTechLocal(_names[i], "Assault Cannon") && (totalUnits()[i] <= 3)) {
                 for(int k = 0; k<_unitCounts[i][Database.CRUISER]; k++){
                     if(diceRoller() >= _unitHitRate[i][Database.CRUISER] - 3){
-                        preFire[i] += 1;
+                        preFire[i]++;
                     }
                 }
             }
         }
         //Applying Assault Cannons
         for(int i = 0; i<2; i++) {
-            int e;
-            if(i == 0){e=1;}
-            else{e=0;}
+            int e = 1 - i;
             for (int k = 0; k < Database.NUM_SHIPS; k++) {
                 while (preFire[i] > 0) {
                     if (_unitCounts[e][k] > 0) {
-                        _unitCounts[e][k] -= 1;
-                        preFire[i] -= 1;
-                        if (k == Database.DREADNOUGHT) {
-                            if (Database.hasTechLocal(_names[i], "Transfabrication")) {
-                                _unitCounts[e][Database.DESTROYER] += 1;
-                                k -= 2;
-                            }
+                        _unitCounts[e][k]--;
+                        preFire[i]--;
+                        if (k == Database.DREADNOUGHT && Database.hasTechLocal(_names[e], "Transfabrication")) {
+                            _unitCounts[e][Database.DESTROYER]++;
+                            k -= 2;
                         }
-                    } else {
-                        break;
-                    }
+                    } else {break;}
                 }
             }
         }
@@ -295,7 +317,7 @@ public class CombatSimTab extends AbstractTab {
             
             if(Database.hasTechLocal(_names[i],"Auxiliary Drones") && _unitCounts[i][Database.DREADNOUGHT] > 0){
                 if(diceRoller() >= (_unitHitRate[i][Database.DREADNOUGHT]) + 3) {
-                    hits[i] += 1;
+                    hits[i]++;
                 }
             }
         }
@@ -308,42 +330,38 @@ public class CombatSimTab extends AbstractTab {
                     if (_unitCounts[e][k] > 0) {
                         _unitCounts[e][k]--;
                         cruiserHits[i]--;
-                        if (k == Database.DREADNOUGHT && Database.hasTechLocal(_names[i], "Transfabrication")) {
+                        if (k == Database.DREADNOUGHT && Database.hasTechLocal(_names[e], "Transfabrication")) {
                             _unitCounts[e][Database.DESTROYER]++;
                             k -= 2;
                         }
-                    } else {
-                        break;
-                    }
+                    } else {break;}
                 }
             }
         }
 
-        //Inflict normal hits on enemy
+        //Inflict normal hits
         for(int i=0; i<2; i++){
             int e = 1 - i;
             while ((DREAD_SUS[e] > 0 || WAR_SUS[e] > 0) && hits[i] > 0) {
                 if (DREAD_SUS[e] > 0) {
-                    DREAD_SUS[e] -= 1;
-                    hits[i] -= 1;
+                    DREAD_SUS[e]--;
+                    hits[i]--;
                 } else if (WAR_SUS[e] > 0) {
-                    WAR_SUS[e] -= 1;
-                    hits[i] -= 1;
+                    WAR_SUS[e]--;
+                    hits[i]--;
                 }
                 else{break;}
             }
             for (int k = 0; k < Database.NUM_SHIPS; k++) {
                 while (hits[i] > 0) {
                     if (_unitCounts[e][k] > 0) {
-                        _unitCounts[e][k] -= 1;
-                        hits[i] -= 1;
+                        _unitCounts[e][k]--;
+                        hits[i]--;
                         if (k == Database.DREADNOUGHT && Database.hasTechLocal(_names[e], "Transfabrication")) {
-                            _unitCounts[e][Database.DESTROYER] += 1;
+                            _unitCounts[e][Database.DESTROYER]++;
                             k -= 2;
                         }
-                    } else {
-                        break;
-                    }
+                    } else {break;}
                 }
             }
         }
@@ -364,13 +382,13 @@ public class CombatSimTab extends AbstractTab {
         for(int i=0; i<2; i++) {
             if (Database.hasTechLocal(_names[i], "Duranium Armor")) {
                 if (_unitCounts[i][Database.WAR_SUN] > WAR_SUS[i]) {
-                    WAR_SUS[i] += 1;
+                    WAR_SUS[i]++;
                 } else if (_unitCounts[i][Database.DREADNOUGHT] > DREAD_SUS[i]) {
-                    DREAD_SUS[i] += 1;
+                    DREAD_SUS[i]++;
                 }
             }
             if(Database.hasTechLocal(_names[i],"Hyper Metabolism")){
-                _unitHitRate[i][Database.DESTROYER] -= 1;
+                _unitHitRate[i][Database.DESTROYER]--;
             }
         }
         return combat();
@@ -389,13 +407,17 @@ public class CombatSimTab extends AbstractTab {
     public String combatSim() {
         //Average remaining units, wins, losses
         Float[] avgUrem = {(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0,(float) 0};
-        try {
-            _names[PLAYER] = _playerName.getText();
-            System.out.println(_names[PLAYER]);
-            _names[ENEMY] = _eOptions.getValue(); // gets the value selected by the combobox
-        } catch (NullPointerException e) {
-            return "Enemy name field is null";
+
+        _names[PLAYER] = _eOptions[PLAYER].getValue();
+        _names[ENEMY] = _eOptions[ENEMY].getValue(); // gets the value selected by the combobox
+
+        if(_names[PLAYER] == null){
+            return "Choose the attacker";
         }
+        if(_names[ENEMY] == null){
+            return "Choose the defender";
+        }
+
         for(int i = 0; i<1000; i++) {
             setUnits();
             //Pre-Combat
@@ -441,16 +463,16 @@ public class CombatSimTab extends AbstractTab {
         avgUrem[10] /= 10;
         avgUrem[11] /= 10;
         avgUrem[12] /= 10;
-        String[] prefix = {"Victory = ", "Defeat = ", "Stalemate = "};
-        String results = "Out of 1000 trials, the results were: \n";
+        String[] prefix = {"Attacker Victory = ", "Defender Victory = ", "Stalemate = "};
+        String results = "\n \nOut of 1000 trials, the results were: \n";
         for(int i=0; i<3; i++){
             results = results.concat(prefix[i]) + (new BigDecimal(Float.toString(avgUrem[i+10])).setScale(1, BigDecimal.ROUND_HALF_EVEN).toString()) + "%\n";
         }
-        results = results.concat("You had the following average remaining units:\n");
+        results = results.concat(_names[PLAYER] + " had the following average remaining units:\n");
         for(int i = 0; i<5; i++){
             results = results.concat(Database.nameOfShip(i) + ": ").concat((new BigDecimal(Float.toString(avgUrem[i])).setScale(2, BigDecimal.ROUND_HALF_EVEN).toString())).concat("\n");
         }
-        results = results.concat("The enemy had the following average remaining units: \n");
+        results = results.concat(_names[ENEMY] + " had the following average remaining units: \n");
         for(int i = 0; i<5; i++){
             results = results.concat(Database.nameOfShip(i) + ": ").concat((new BigDecimal(Float.toString(avgUrem[i+5])).setScale(2, BigDecimal.ROUND_HALF_EVEN).toString())).concat("\n");
         }
